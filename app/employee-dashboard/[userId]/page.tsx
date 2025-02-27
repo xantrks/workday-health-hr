090de65a6e3c33d5a5e48380334a3da0319fcd34
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-02-28 02:54:50 +1300

    Remove redundant and useless code

--- app/employee-dashboard/[userId]/page.tsx
+++ app/employee-dashboard/[userId]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
 export default function EmployeeDashboard({ params }: { params: { userId: string } }) {
